8fded2343d6676158349c287a5cdbdb559b4c6f4
violoncello-ver <hasema.engineer@gmail.com>
2026-01-16 11:20:17 +0900

    プログラミング入門２

--- 1-2.test-java-entry2/src/Exam1.java
+++ 1-2.test-java-entry2/src/Exam1.java
@@ -1,16 +1,16 @@
 public class Exam1 {
     public static void main(String[] args) {
         
-        int totalScore = 48;
+        int totalScore = 101;
         
-        if (totalScore <= 79) {
+        if (totalScore > 100 || totalScore < 0) {
+            System.out.println("存在しない点数です");
+        } else if (totalScore <= 79) {
             System.out.println("追試です");
         } else if (totalScore == 100) {
             System.out.println("満点です");
         } else if (totalScore >= 80 ) {
                 System.out.println("合格です");
-        } else {
-            System.out.println("存在しない点数です");
         }
     }
 }
